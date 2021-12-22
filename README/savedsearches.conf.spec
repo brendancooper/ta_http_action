2efd5800a4d5e0a43aa76322f94d8cfae41d3d19
Benjamin Keller <benjamin@keller-datainsights.ch>
2021-12-22 13:38:26 +0100

    Added Proxy support

--- README/savedsearches.conf.spec
+++ README/savedsearches.conf.spec
@@ -1,8 +1,8 @@
 
 [httpalert]
 httpalert.param.endpoint = <string> Endpoint. It's a required parameter.
-httpalert.param.qs_params = <string> Querystring Params.
-httpalert.param.custom_headers = <string> Headers.
-httpalert.param.payload = <string> Body.
+httpalert.param.qs_params = <string> Querystring Params. Use Comma , for separation and Key=Value.
+httpalert.param.custom_headers = <string> Headers. Use Comma , for separation and Key=Value.
+httpalert.param.payload = <string> Body. 
 httpalert.param.method = <list> Method. It's a required parameter. It's default value is post.
-
+httpalert.param.proxy = <string> Proxy to reach endpoint.
--- bin/http_alert.py
+++ bin/http_alert.py
@@ -1,4 +1,3 @@
-
 # encoding = utf-8
 import logging
 import json
@@ -27,7 +26,7 @@ def helper_get_requests_func(method):
     elif method == "put":
         return requests.put
 
-def process(data):    
+def process(data):
     configuration = data['configuration']
     results = data['result']
 
@@ -35,27 +34,41 @@ def process(data):
     input_custom_headers = configuration.get('custom_headers')
     payload = configuration.get('payload')
     method = configuration.get('method')
-    qs_params = configuration.get('qs_params')
+    qs_params = {}
+    input_qs_params = configuration.get('qs_params')
+    proxies = {}
+    proxies['https'] = configuration.get('proxy')
     timeout = 30
     custom_headers = {}
     requests_func = helper_get_requests_func(method)
 
     if input_custom_headers is not None and len(input_custom_headers) > 0:
-        header_dict = input_custom_headers.split("&")
+        header_dict = input_custom_headers.split(',')
         if len(header_dict) > 0:
             for value in header_dict:
-                key, value = value.split("=")
+                key, value = value.split('=')
                 custom_headers[key]=value
     else:
         custom_headers=None
+
+    if input_qs_params is not None and len(input_qs_params) > 0:
+        #logging.error("input_qs_params %s" % input_qs_params)
+        params_dict = input_qs_params.split(',')
+        #logging.error("params_dict %s" % params_dict)
+        if len(params_dict) > 0:
+            for value in params_dict:
+                key, value = value.split('=')
+                qs_params[key]=value
+            logging.error("Params %s" % json.dumps(qs_params))
+    else:
+        qs_params=None
 
     if not endpoint.startswith("https://"):
         logging.info("ERROR httpalert job={} endpoint={} endpoint does not start https:// ".format(data['sid'],endpoint))
         return
 
-    response = requests_func(endpoint, params=qs_params, data=payload, headers=custom_headers, timeout=timeout)
+    response = requests_func(endpoint, params=qs_params, data=payload, headers=custom_headers, timeout=timeout,proxies=proxies)
     logging.info("httpalert Job={} endpoint={} status_code={} ".format(data['sid'],endpoint,response.status_code))
-    
 
 if len(sys.argv) > 1 and sys.argv[1] == "--execute":
     data = json.load(sys.stdin)
@@ -76,4 +89,3 @@ if len(sys.argv) > 1 and sys.argv[1] == "--test":
         }
     }
     process(data)
-
